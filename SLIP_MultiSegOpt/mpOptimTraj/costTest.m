0a26a81f7098375f4a8e1bbbb6533ed5d74850b1
Kenneth Chao <slongz@tamu.edu>
2018-08-29 08:35:14 -0500

    Starting testing multi phase

---
+++ SLIP_MultiSegOpt/mpOptimTraj/costTest.m
@@ -0,0 +1,9 @@
+function result = gcostTest(xVec)
+%COSTTEST Summary of this function goes here
+%   Detailed explanation goes here
+result = ones(size(xVec));
+
+
+
+end
+
---
+++ SLIP_MultiSegOpt/mpOptimTraj/gcostTest.m
@@ -0,0 +1,6 @@
+function cost = costTest(xVec)
+%COSTTEST Summary of this function goes here
+%   Detailed explanation goes here
+cost = sum(xVec);
+end
+
